dc68393fc2ac07ac88830bb0969cc201d3a845f6
wedojava <wedojava@gmail.com>
2016-02-14 18:07:01 +0800

    add and mix js and css

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,46 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
 var elixir = require('laravel-elixir');
+
+/**
+ * 拷贝任何需要的文件
+ *
+ * Do a 'gulp copyfiles' after bower updates
+ */
+gulp.task("copyfiles", function() {
+
+    gulp.src("vendor/bower_dl/jquery/dist/jquery.js")
+        .pipe(gulp.dest("resources/assets/js/"));
+
+    gulp.src("vendor/bower_dl/bootstrap/less/**")
+        .pipe(gulp.dest("resources/assets/less/bootstrap"));
+
+    gulp.src("vendor/bower_dl/bootstrap/dist/js/bootstrap.js")
+        .pipe(gulp.dest("resources/assets/js/"));
+
+    gulp.src("vendor/bower_dl/bootstrap/dist/fonts/**")
+        .pipe(gulp.dest("public/assets/fonts"));
+
+    gulp.src("vendor/bower_dl/font-awesome/less/**")
+        .pipe(gulp.dest("resources/assets/less/fontawesome"));
+
+    gulp.src("vendor/bower_dl/font-awesome/fonts/**")
+        .pipe(gulp.dest("public/assets/fonts"));
+
+    // 拷贝 datatables
+    var dtDir = 'vendor/bower_dl/datatables-plugins/integration/';
+
+    gulp.src("vendor/bower_dl/datatables/media/js/jquery.dataTables.js")
+        .pipe(gulp.dest('resources/assets/js/'));
+
+    gulp.src(dtDir + 'bootstrap/3/dataTables.bootstrap.css')
+        .pipe(rename('dataTables.bootstrap.less'))
+        .pipe(gulp.dest('resources/assets/less/others/'));
+
+    gulp.src(dtDir + 'bootstrap/3/dataTables.bootstrap.js')
+        .pipe(gulp.dest('resources/assets/js/'));
+
+});
 
 /*
  |--------------------------------------------------------------------------
@@ -12,5 +54,19 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    // mix.sass('app.scss');
+    // 
+    // 合并脚本文件
+    mix.scripts([
+            'js/jquery.js',
+            'js/bootstrap.js',
+            'js/jquery.dataTables.js',
+            'js/dataTables.bootstrap.js'
+        ],
+        'public/assets/js/lvchuangke.js',
+        'resources/assets'
+    );
+
+    // 编译 Less
+    mix.less('lvchuangke.less', 'public/assets/css/lvchuangke.css');
 });
