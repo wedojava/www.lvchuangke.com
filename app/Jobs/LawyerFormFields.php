29cf1acef257e76dfdcdc51397bc8a72cb5b3272
wedojava <wedojava@gmail.com>
2016-02-25 12:25:53 +0800

    Add LawyerJob for form fields.

---
+++ app/Jobs/LawyerFormFields.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Jobs\Job;
+use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+// 这个 job 将在我们获取表单内容并将之填充到 form 的时候起作用
+class LawyerFormFields extends Job implements SelfHandling
+{
+    /**
+     * The id(if any) of the Post row.
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * List of fields and default value for each field
+     * @var array
+     */
+    protected $fieldList = [
+        'username' => '',
+        'telephone' => '',
+        'email' => '',
+        'QQ' => '',
+        'description' => '',
+    ];
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($id = null)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $fields = $this->fieldList;
+
+        // 如果 id 不是 null，通过模型返回 id 是 $id 的 array，并跳出 job.
+        // 如果 id 是 null,说明可能处于创建数据，此时如果提交失败，填充数据应当为request里的内容，即适用old()调用
+        if ($this->id) {
+            $fields = $this->fieldsFromModel($this->id, $fields);
+        }
+
+        // id 是 null，遍历 fieldList 内容,设定$fields的键值对为old键值。
+        foreach ($fieldList as $fieldName => $fieldValue) {
+            $fields[$fieldName] = old($fieldName, $fieldValue);
+        }
+
+        return array_merge(
+            $fields
+        );
+    }
+
+    /**
+     * Return the field values from the model
+     * @param  integer $id     Post ID
+     * @param  array  $fields postModelFieldList
+     * @return array         $fieldsList
+     */
+    public function fieldsFromModel($id, array $fields)
+    {
+        $lawyer = Lawyer::findOrFail($id);
+
+        $fieldNames = array_keys($fields);
+
+        $field = ['id' => $id];
+
+        foreach ($fieldNames as $field) {
+            $fields[$field] = $lawyer->{$field};
+        }
+
+        return $fields;
+    }
+}
