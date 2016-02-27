2d2bce7343e4e3627379443f23778ddd5c7d2fef
wedojava <wedojava@gmail.com>
2016-02-27 10:05:19 +0800

    从后台调取关于信息

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,7 @@
     <div class="jumbotron">
       <div class="row">
         <div class="col-md-8">
-          <h1>我们承诺</h1>
-          <p>你还在东奔西跑，自己找律师吗？而且，你找的只是<span class="lck-highlight">一个</span>律师？太 Out 了！</p>
-          <p>我们为一个案件推荐该领域内最权威的<span class="lck-highlight">多名</span>律师，由众多律师分别给出答辩意见书，再有我们专业团队进行筛查，进入案件预审程序，最终则最优律师留下。</p>
-          <p>我们为您保驾护航！我们承诺，最终，你只需要话费请一名律师的钱！</p>
-          <p>
-            <a class="btn btn-lg btn-lck-blue" href="javascript:;" role="button">联系我们 <i class="fa fa-btn fa-arrow-right"></i>15899138838</a>
-          </p>
+            {!! App\About::first()->detail_html !!}
         </div>
         <div class="col-md-4">
           <form class="form-horizontal" method="POST" action="/guestcase" enctype="multipart/form-data">
