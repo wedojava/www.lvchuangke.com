a0179af6f43f54b9465ca310f96315eb3166fa0f
wedojava <wedojava@gmail.com>
2016-03-01 12:05:47 +0800

    add default string of about detail_raw in seeder.

--- database/seeds/AboutTableSeeder.php
+++ database/seeds/AboutTableSeeder.php
@@ -11,7 +11,23 @@ class AboutTableSeeder extends Seeder
      */
     public function run()
     {
-        App\About::truncate();
-        factory(App\About::class, 1)->create();
+        $detail_raw = <<<EOT
+#我们承诺
+
+你还在东奔西跑，自己找律师吗？而且，你找的只是<span class="lck-highlight">一个</span>律师？太 Out 了！
+
+我们为一个案件推荐该领域内最权威的<span class="lck-highlight">多名</span>律师，由众多律师分别给出答辩意见书，再有我们专业团队进行筛查，进入案件预审程序，最终则最优律师留下。
+
+我们为您保驾护航！我们承诺，最终，你只需要话费请一名律师的钱！
+
+<p>
+    <a class="btn btn-lg btn-lck-blue" href="javascript:;" role="button">联系我们 <i class="fa fa-btn fa-arrow-right"></i>15899138838</a>
+</p>
+EOT;
+        $data = [
+            'detail_raw' => $detail_raw
+
+        ];
+        App\About::create($data);
     }
 }
