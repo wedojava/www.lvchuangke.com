1ae70966f21c1559448681a9aeba921498bc7559
wedojava <wedojava@gmail.com>
2016-03-02 19:58:25 +0800

    show successfulCases in welcome from db.

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,6 +6,7 @@ use App\About;
 use App\Http\Controllers\Controller;
 use App\Http\Requests;
 use App\LawOffice;
+use App\SuccessfulCase;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
@@ -15,6 +16,7 @@ class WelcomeController extends Controller
     	// flash()->overlay('Success!', '你的情况已经提交成功，我们稍后会和您取得联系，请您保持电话通畅！');
     	return view('welcome')
     		->withAbout(About::first())
-    		->withLawOffices(LawOffice::take(9)->get());
+    		->withLawOffices(LawOffice::take(9)->get())
+    		->withSuccessfulCases(SuccessfulCase::take(9)->get());
     }
 }
--- resources/views/welcome/successfulcases.blade.php
+++ resources/views/welcome/successfulcases.blade.php
@@ -5,7 +5,10 @@
 	    </h2>
 		<div class="row">
 			<ul class="lck-list">
-				<li class="lck-list__element"><i class="fa fa-check"></i> <a href="">Automotive & Vehicle</a></li>
+				@foreach($successful_cases->all() as $successful_case)
+					<li class="lck-list__element"><i class="fa fa-check"></i> <a href="">{{$successful_case->caseTitle}}</a></li>
+				@endforeach
+				{{-- <li class="lck-list__element"><i class="fa fa-check"></i> <a href="">Automotive & Vehicle</a></li>
 				<li class="lck-list__element"><i class="fa fa-check"></i> <a href="">Enviromental & Green</a></li>
 				<li class="lck-list__element"><i class="fa fa-check"></i> <a href="">Real Estate & Mortgage</a></li>
 				<li class="lck-list__element"><i class="fa fa-check"></i> <a href="">Staff Favourites</a></li>
@@ -13,7 +16,7 @@
 				<li class="lck-list__element"><i class="fa fa-check"></i> <a href="">Community & Non-Profit</a></li>
 				<li class="lck-list__element"><i class="fa fa-check"></i> <a href="">Entertainment & The Arts</a></li>
 				<li class="lck-list__element"><i class="fa fa-check"></i> <a href="">Wedding Services</a></li>
-				<li class="lck-list__element"><i class="fa fa-check"></i> <a href="">Medical & Pharmaceutical</a></li>
+				<li class="lck-list__element"><i class="fa fa-check"></i> <a href="">Medical & Pharmaceutical</a></li> --}}
 			</ul>
 			<div class="front-section__signup awesome__button">
 	            <a href="/successfulcases" class="btn--monster">更多案例</a>
