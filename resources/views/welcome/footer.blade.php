ffbb01b09d2852acb2bcc93fd95ac829c08cbf2a
wedojava <wedojava@gmail.com>
2016-03-03 20:01:20 +0800

    备案修改

--- resources/views/welcome/footer.blade.php
+++ resources/views/welcome/footer.blade.php
@@ -2,11 +2,11 @@
 	<div class="container">
 	<p>© Lvchuangke 2016. &nbsp;&nbsp; All rights reserved. &nbsp;&nbsp;		
 		Proudly hosted with <a href="#">aliyun</a> &nbsp;&nbsp;
-		<a href="#">备案:12341234</a>
+		<a href="http://www.miitbeian.gov.cn/">新ICP备16000503号-1</a>
 	</p>
 	<p>
 		Power By Laravel 5.2 & bootstrap 3 &nbsp;&nbsp;
-		WEB Designer:<a href="http://huifeng.me">Huifeng.Me</a>&nbsp;&nbsp;
+		Made By:<a href="http://huifeng.me">Huifeng.Me</a>&nbsp;&nbsp;
 		<a href="https://github.com/wedojava/www.lvchuangke.com" class="footer-link" target="_blank"><i class="fa fa-github-alt"></i></a>&nbsp;&nbsp;
 		Licensed under <a href="http://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>
 	</p>
